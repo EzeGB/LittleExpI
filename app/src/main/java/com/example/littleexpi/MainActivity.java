7c607e9b17f9caae60692524e8a743dede9784ab
Zeke <ezequielgerstel777@gmail.com>
2025-01-13 15:25:14 -0400

    logical commit

--- app/src/main/java/com/example/littleexpi/MainActivity.java
+++ app/src/main/java/com/example/littleexpi/MainActivity.java
@@ -2,7 +2,9 @@ package com.example.littleexpi;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
@@ -24,8 +26,36 @@ public class MainActivity extends AppCompatActivity {
             return insets;
         });
     }
+
+    //it displays an emergent message with the name of the favorite band at the bottom
     public void showBand (View v){
         String bandput = ((EditText)findViewById(R.id.editTextText)).getText().toString();
         Toast.makeText(this, bandput, Toast.LENGTH_LONG).show();
+    }
+
+    //it enables or disables the SHOW button next to it
+    public void toggleShowButton (View v){
+        Button showButton = ((Button)findViewById(R.id.button2));
+        if (showButton.isEnabled()){
+            showButton.setEnabled(false);
+        } else {
+            showButton.setEnabled(true);
+        }
+    }
+
+    //it sums the value of the button with the current sum
+    public void add (View v){
+        Button button = (Button) v;
+        TextView currentResult = findViewById(R.id.textView3);
+        int addend1 = Integer.parseInt(currentResult.getText().toString());
+        int addend2 = Integer.parseInt(button.getText().toString());
+        int newResult = addend1 +addend2;
+        currentResult.setText(String.valueOf(newResult));
+    }
+
+    //it resets the value of the sum to 0
+    public void resetSum (View v){
+        TextView sum = findViewById(R.id.textView3);
+        sum.setText("0");
     }
 }
